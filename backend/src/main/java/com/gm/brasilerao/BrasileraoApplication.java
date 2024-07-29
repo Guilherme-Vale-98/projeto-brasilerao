3e07c58fa5d0fa4a95d1cf4444cfdfad43ca24fd
Guilherme Vale <guilherme.vale98@gmail.com>
2024-07-29 11:27:57 -0300

    first commit

---
+++ backend/src/main/java/com/gm/brasilerao/BrasileraoApplication.java
@@ -0,0 +1,13 @@
+package com.gm.brasilerao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileraoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gm/brasilerao/BrasileraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.brasilerao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
