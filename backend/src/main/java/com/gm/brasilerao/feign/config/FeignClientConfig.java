65111f67d4e2229e236390e5e5839d17d65d8fb0
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-04 19:38:55 -0300

    Change FeignClientConfig

--- backend/src/main/java/com/gm/brasilerao/feign/config/FeignClientConfig.java
+++ backend/src/main/java/com/gm/brasilerao/feign/config/FeignClientConfig.java
@@ -2,17 +2,19 @@ package com.gm.brasilerao.feign.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignClientConfig {
+    @Value("${gm.token}")
+    String token;
     @Bean
     public RequestInterceptor requestInterceptor() {
         return new RequestInterceptor() {
             @Override
             public void apply(RequestTemplate template) {
-                String token = System.getenv("TOKEN");
                 if (token != null && !token.isEmpty()) {
                     template.header("X-Auth-Token", token);
                 }
