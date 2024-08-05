f4d2a501c0a3aec03f761a9f3653ad9beaa6ff7d
Guilherme Vale <guilherme.vale98@gmail.com>
2024-08-05 17:48:35 -0300

    add Hero

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import estadio from '../img/estadio.jpg'
+import brasao from '../img/brasao.png'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+    return (
+        <section className='relative  flex'>
+            <div className='-z-10 absolute w-full h-[330px] '>
+                <Image src={estadio}
+                    layout="fill"
+                    quality={100} alt='estádio' />
+            </div>
+            <div className='w-full hidden sm:flex justify-end '>
+                    <Image src={brasao} alt='brasao do campeonato' width={600} quality={100}></Image>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,12 +8,12 @@ type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <nav className="bg-green-800">
+        <nav className="bg-[#125f12]">
             <div className='p-3 mx-auto w-[80%] flex items-center  text-3xl font-semibold text-white'>
                 <Link href='/'>
                     <Image src={logo} alt='TraversyPress' width={40} />
                 </Link>
-                <span className='mt-2 ml-3'>Brasileirão</span>
+                <span className='mt-1 ml-3'>Brasileirão</span>
             </div>
         </nav>
     )
